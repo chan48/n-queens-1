060ad5755a57cd1d034c1dcff37912976725903d
dsernst <github@dsernst.com>
2014-12-17 16:53:56 -0800

    non-working findNRooksSolution

--- src/Board.js
+++ src/Board.js
@@ -164,12 +164,12 @@
 
   });
 
-  var makeEmptyMatrix = function(n) {
-    return _(_.range(n)).map(function() {
-      return _(_.range(n)).map(function() {
-        return 0;
-      });
-    });
-  };
-
 }());
+
+var makeEmptyMatrix = function(n) {
+  return _(_.range(n)).map(function() {
+    return _(_.range(n)).map(function() {
+      return 0;
+    });
+  });
+};
--- src/solvers.js
+++ src/solvers.js
@@ -14,13 +14,36 @@
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
 window.findNRooksSolution = function(n) {
-  var solution = null; //fixme
-
   var board = new Board({n:n});
+  var solution = null;
+  if (n < 1) {
+    console.log('eff yeah');
+    return null;
+  }
 
+  var rootTree = function(rowNum, colNum) {
+    if(colNum === n) {
+      //no possibilities on this row
+      return null;
+    } else if (rowNum === n) {
+      //finished the board
+      solution = board.rows();
+      return solution;
+    }
+
+    board.togglePiece(rowNum,colNum);
+
+    if(!board.hasAnyRooksConflicts()) {
+      rootTree(rowNum+1,0);
+    } else {
+      board.togglePiece(rowNum,colNum);
+      rootTree(rowNum,colNum+1);
+    }
+  };
 
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
+
+  return rootTree(0,0);
 };
 
 
