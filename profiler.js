d0a4292cbf9bcd80a2e0dfce5717a8b8f45b04eb
dsernst <github@dsernst.com>
2014-12-17 19:48:59 -0800

    cleanup profiler

--- profiler.js
+++ profiler.js
@@ -6,9 +6,9 @@ console.log(variant + tests + " tests");
 
 console.time('total');
 for (var example = 0; example < tests; example++){
-  console.time("time " + example);
+  console.time("test " + (example + 1));
   console.log('solutions for 8: ' + countNQueensSolutions(8))
-  console.timeEnd("time " + example);
+  console.timeEnd("test " + (example + 1));
 }
 
 console.log('');
